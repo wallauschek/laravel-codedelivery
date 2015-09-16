54bc1cc5800afb03b401ab2e2bfa6e5493344db0
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2015-09-16 16:23:16 -0300

    aula 1

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CodeDelivery\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'order_id',
+        'price',
+        'qtd'
+    ];
+
+    public function products(){
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+}
---
+++ database/seeds/clientTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use CodeDelivery\Models\Category;
+use CodeDelivery\Models\Product;
+use Illuminate\Database\Seeder;
+
+class clientTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory()
+    }
+}
