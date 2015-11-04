f31534a7a19637b2dd8974168d4bc1cf7a9e0b30
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2015-11-04 07:43:47 -0200

    aulas OAuth2 e API Client

---
+++ app/Http/Controllers/Api/Client/ClientCheckoutController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace CodeDelivery\Http\Controllers\Api\Client;
+
+use CodeDelivery\Repositories\OrderRepository;
+use CodeDelivery\Repositories\UserRepository;
+use CodeDelivery\Repositories\ProductRepository;
+use CodeDelivery\Services\OrderService;
+use Illuminate\Http\Request;
+use CodeDelivery\Http\Requests;
+use CodeDelivery\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use LucaDegasperi\OAuth2Server\Facades\Authorizer;
+
+class ClientCheckoutController extends Controller
+{
+    public function __construct(
+        OrderRepository $repository,
+        UserRepository $userRepository,
+        ProductRepository $productRepository,
+        OrderService $service
+
+        ){
+        $this->repository = $repository;
+        $this->userRepository = $userRepository;
+        $this->productRepository = $productRepository;
+        $this->service = $service;
+    }
+
+    public function index(){
+        $clientId = Authorizer::getResourceOwnerId();
+        $orders = $this->repository->scopeQuery(function($query) use($clientId){
+            return $query->with('items')->where('client_id', '=', $clientId);
+        })->paginate();
+
+        return $orders;
+    }
+
+
+    public function store(Request $request){
+        $data = $request->all();
+        $id = Authorizer::getResourceOwnerId();
+        $clientId = $this->userRepository->find($id)->client->id;
+        $data['client_id'] = $clientId;
+        $o = $this->service->create($data);
+        $o = $this->repository->with('items')->find($o->id);
+
+        return $o;
+    }
+
+    public function  show($store){
+        $o = $this->repository->with(['client', 'items', 'cupom'])->find($store);
+        $o->items->each(function($item){
+            $item->product;
+        });
+        $o->client->user;
+
+        return $o;
+    }
+}
+
